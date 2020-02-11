284197642994ab252f550e209a1b85a738449b1f
jmorango <joseamed@gmail.com>
2020-02-11 13:39:30 -0500

    loaded template
    loaded the template for the log in / sig up page.

--- geekprofile/views.py
+++ geekprofile/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 #create views
-def profile_detail(request, id):
-    return HttpResponse('<p>Profile with the id {}</p>'.format(id))
+def profile_detail(request):
+    return render(request, 'profile_home.html')
+
+    #HttpResponse('<p>Profile with the id {}</p>'.format(id))
