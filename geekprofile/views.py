cf392139931575214e33b38908fc69b51adca4dd
jmorango <joseamed@gmail.com>
2020-02-11 09:50:53 -0500

    change url.py view
    Since all apps have a views.py when calling on url.py have to use from app import views as new_views_name.

--- geekprofile/views.py
+++ geekprofile/views.py
@@ -1,17 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from details.models import Book
-
-def home(request):
-    books = Book.objects.all()
-    return render(request, 'home.html', {'books': books})
-
+#create views
 def profile_detail(request, id):
     return HttpResponse('<p>Profile with the id {}</p>'.format(id))
-
-def book_detail(request, id):
-    try:
-        book = Book.objects.get(id=id)
-    except Book.DoesNotExist:
-        raise Http404('Book Not Found')
-    return render(request, 'book_detail.html', {'book': book})
