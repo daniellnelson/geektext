3ad57a428235810fc8edcf1e6b97cef758740612
jmorango <joseamed@gmail.com>
2020-02-11 10:35:41 -0500

    Added User Class
    registered Profile, added User class to the data base. ,

--- geekprofile/models.py
+++ geekprofile/models.py
@@ -3,25 +3,23 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length = 100)
-    password = models.CharField(max_length = 50)
-    email    = models.EmailField(blank = False)
-    home_address = models.ManyToManyField('Address', null = False)
+    home_address = models.ManyToManyField('Address')
     nickname = models.CharField(max_length = 25)
     creditcard = models.ForeignKey('CreditCard',on_delete=models.CASCADE)
-    
-    
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+
 class Address(models.Model):
     street_addr = models.CharField(max_length = 100)
     apt_suite_unit = models.CharField(max_length = 25,blank = True)
     city = models.CharField(max_length = 50)
     state = models.CharField(max_length = 25)
     zipcode = models.CharField(max_length = 5)
-    
+
 class CreditCard(models.Model):
     name = models.CharField(max_length = 100)
     number = models.CharField(max_length = 16)
     expire = models.CharField(max_length = 7)
     code   = models.CharField(max_length = 4)
     zipcode = models.CharField(max_length = 5)
-    
--- geekprofile/views.py
+++ geekprofile/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 #create views
 def profile_detail(request, id):
     return HttpResponse('<p>Profile with the id {}</p>'.format(id))
