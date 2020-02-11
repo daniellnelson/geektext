8bc0c639df98f675e9742e026a1c1b3b323fa435
jmorango <joseamed@gmail.com>
2020-02-10 21:37:56 -0500

    profile classes
    address class , credit card class and user class. needs to look into shipping address and verifying email , credit card and physical address

--- geekprofile/models.py
+++ geekprofile/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 50)
+    email    = models.EmailField(blank = False)
+    home_address = models.ManyToManyField('Address', null = False)
+    nickname = models.CharField(max_length = 25)
+    creditcard = models.ForeignKey('CreditCard',on_delete=models.CASCADE)
+    
+    
+class Address(models.Model):
+    street_addr = models.CharField(max_length = 100)
+    apt_suite_unit = models.CharField(max_length = 25,blank = True)
+    city = models.CharField(max_length = 50)
+    state = models.CharField(max_length = 25)
+    zipcode = models.CharField(max_length = 5)
+    
+class CreditCard(models.Model):
+    name = models.CharField(max_length = 100)
+    number = models.CharField(max_length = 16)
+    expire = models.CharField(max_length = 7)
+    code   = models.CharField(max_length = 4)
+    zipcode = models.CharField(max_length = 5)
+    
