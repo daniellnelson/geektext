65d3d9b62c0f4692a276074d9e63a17ae2158683
jmorango <joseamed@gmail.com>
2020-02-27 08:11:08 -0500

    reverting
    Reverting wish list and shopping cart changes with also javascript code

--- Shopping_cart/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
-from .models import Item
-def item_list(request):
-    context = {
-        'items' : Item.objects.all()
-    }
-    #return render(request, "checkout-page.html", context)
-    return render(request, "checkout.html", context)
--- wishlist/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-from .models import Wishlist
-
-def wish_list(request):
-    lists = Wishlist.objects.all()
-    return render(request, 'wishlist.html', {'lists': lists})
