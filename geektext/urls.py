d3223892bb791355f3f212f4b1ba047f60afb893
Wendy Orellana <worel001@fiu.edu>
2020-03-06 12:31:53 -0500

    Updated ratings

--- geektext/urls.py
+++ geektext/urls.py
@@ -12,7 +12,7 @@ from django.urls import path, include
 
 from Shopping_cart import views as cart_views
 
-
+from ratings import views as ratings_views
 
 
 urlpatterns = [
@@ -27,6 +27,9 @@ urlpatterns = [
     url(r'wishlist/current/(\d+)/', wishviews.current_wish_list, name='current_wish_list'),
     url(r'^profile/', profile_views.profile_detail, name = 'profile_detail'),
     url(r'^shopping_cart/', cart_views.item_list, name = 'item-list'),
+    url(r'^details/review/(\d+)/', ratings_views.write_comment, name = 'write_comment'),
+    url(r'^details/rate/(\d+)/', ratings_views.rate_book, name = 'rate_book'),
+    url(r'^reviews/(\d+)/', ratings_views.display_comment, name = 'display_comment'),
 ]
 
 
