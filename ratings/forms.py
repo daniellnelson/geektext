2eabe33a332bfb414bb89e68aeece0afad41eb05
Wendy Orellana <worel001@fiu.edu>
2020-03-20 23:52:34 -0400

    Update forms.py

--- ratings/forms.py
+++ ratings/forms.py
@@ -10,11 +10,11 @@ class ReviewForm(forms.ModelForm):
             'anonymous': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
             'rating': forms.Select(attrs={'class':  'form-control'}, choices=(
                 (None, 'None'),
-                (1, 'Poor'),
-                (2, 'Average'),
-                (3, 'Good'),
-                (4, 'Very Good'),
-                (5, 'Excellent')
+                (1, 'Poor (1)'),
+                (2, 'Average (2)'),
+                (3, 'Good (3)'),
+                (4, 'Very Good (4)'),
+                (5, 'Excellent (5)')
             )),
             'comment': forms.Textarea(attrs={'class': 'form-control'}),
         }
