51f62c511bc2033b0e3629c20c6066644d6d4abf
daniellnelson <danielnelson04@hotmail.com>
2020-03-23 12:47:29 -0400

    oops

--- details/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'cover', 'author','genre', 'condition', 'type', 'cost', 'pages']
--- details/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookdetailsConfig(AppConfig):
-    name = 'details'
