0c380326222f2bf3394cc9af0e423a3d028b84d0
FerMoha2177 <fmoha017@fiu.edu>
2020-03-26 18:14:41 -0400

    Added Menu functionality
    a href now redirect to other pages when click from the shopping cart screen

--- Shopping_cart/models.py
+++ Shopping_cart/models.py
@@ -1,23 +1,32 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from datetime import datetime
 from django.conf import settings
 from django.db import models
-from geekprofile.models import Profile
-from details.models import Book
+from django.db.models import Sum
+from geekprofile.models import Profile  #Use from geekprofile
+from details.models import Book       #Use from details
+from django.shortcuts import reverse
+#from django_countries.fields import CountryField
 
-#Use from geekprofile
+
 
 
 class Item(models.Model):
     title = Book.title
     price = Book.cost
     isbn  = Book.ISBN
+    quantity = models.IntegerField(default=1)
+
 
     
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("") #add slug
 
 
 class OrderItem(models.Model):
@@ -25,6 +34,9 @@ class OrderItem(models.Model):
     is_ordered = models.BooleanField(default=False)
     date_added = models.DateTimeField(auto_now=True)
     date_ordered = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.item.name
 
 
    
@@ -34,6 +46,8 @@ class OrderItem(models.Model):
 class Order(models.Model):
 
     #associate the order with a user
+    ref_code = models.CharField(max_length=15, null=True)
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
 
@@ -46,6 +60,28 @@ class Order(models.Model):
     
     #checks if order was ordered
     ordered = models.BooleanField(default=False)
+
+    def get_cart_items(self):
+        return self.items.all()
     
+    def get_cart_total(self):
+        return sum([item.product.price for item in self.items.all()])
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.owner, self.ref_code)
+
+    class Transaction(models.Model):
+        profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+        token = models.CharField(max_length=120)
+        order_id = models.CharField(max_length=120)
+        amount = models.DecimalField(max_digits=100, decimal_places=2)
+        success = models.BooleanField(default=True)
+        timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+        def __str__(self):
+            return self.order_id
+"""
+    class Meta:
+        ordering = ['-timestamp']
+        """
     
-  
